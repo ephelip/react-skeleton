b1865c36ddde43fd6ac5e579afc6dad6b4b90d34
ephelip <etienne.phelip@gmail.com>
2016-02-27 17:29:48 -0500

    add express server

---
+++ server/server.js
@@ -0,0 +1,66 @@
+//Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+//Allow all requests from all domains & localhost
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET");
+  next();
+});
+
+//Setup
+var PORT = process.env.PORT || 6060;
+
+//MiddleWares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var ingredients = [
+    {
+        "id": "234kjw",
+        "text": "Eggs"
+    },
+    {
+        "id": "as82w",
+        "text": "Milk"
+    },
+    {
+        "id": "234sk1",
+        "text": "Bacon"
+    },
+    {
+        "id": "ppo3j3",
+        "text": "Frog Legs"
+    }
+];
+
+//Routes
+app.get('/', function(req, res){
+  res.json({
+    content : "You hit the base route. If you can read this, then the Express Server is working as intended."
+  });
+});
+
+app.get('/ingredients', function(req, res) {
+    console.log("GET From SERVER");
+    res.send(ingredients);
+});
+
+app.post('/ingredients', function(req, res) {
+    var ingredient = req.body;
+    console.log(req.body);
+    ingredients.push(ingredient);
+    res.status(200).send("Successfully posted ingredient");
+});
+
+//Server start
+app.listen(PORT, function(err){
+  if(err){
+    console.log(err);
+  }else{
+    console.log('Express server listening on port '+PORT);
+  }
+});
